f6ab15441155fcde072fee803654f6f1a68fd37f
roxy0507 <roxy.0507@gmail.com>
2021-04-19 16:56:55 -0600

    Add relationships in models

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,2 +1,4 @@
 class Bookmark < ApplicationRecord
+    belongs_to :category
+    belongs_to :kind
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,5 @@
 class Category < ApplicationRecord
+    has_many :bookmark
+    has_many :categories, dependent: :destroy
+    belongs_to :parent, class_name: "Category", optional: true, foreign_key: :category_id
 end
